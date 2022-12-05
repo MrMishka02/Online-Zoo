cfe2dbdf12891a8e3c297ee2927affe3933c18ff
MrMishka02 <m.gogia@hotmail.com>
2022-12-05 21:13:14 +0400

    docs: fixed some bugs

--- pages/petstory/petstory.js
+++ pages/petstory/petstory.js
@@ -1,11 +1,19 @@
+alert("Hello, I'm currently working on week 4!")
+
 const burgerBtn = document.getElementsByClassName("burger_icon")[0];
 burgerBtn.onclick = function burger(){
     document.getElementById("mynav").style.display = "block";
     document.getElementsByClassName("burger_logo")[0].style.display = "block";
     document.getElementsByClassName("circle-h2-btn")[0].style.display = "none";
+    const darked = document.createElement("div");
+    darked.className = "darked";
+    document.body.appendChild(darked);
 }
 const closeBtn = document.getElementsByClassName("cross")[0];
 closeBtn.onclick = function closeBurger(){
     document.getElementById("mynav").style.display = "none";
     document.getElementsByClassName("circle-h2-btn")[0].style.display = "flex";
+    const darkBack = document.getElementsByClassName("darked")[0];
+    document.body.removeChild(darkBack);
+    location.reload();
 }
